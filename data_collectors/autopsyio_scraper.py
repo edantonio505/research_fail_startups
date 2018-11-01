8b47fceb904c161e86e1a4bd417e6b2a79949ca2
Edgar Velazquez <edantonio505@gmail.com>
2018-11-01 03:31:10 -0400

    saving links

--- data_collectors/autopsyio_scraper.py
+++ data_collectors/autopsyio_scraper.py
@@ -7,13 +7,50 @@ import pandas as pd
 # ===================================================================
 def get_df(tr_array):
     dataset = []
+
     for tr in tr_array:
         tds = tr.find_all('td')
-        columns = []
+        columns = []        
+        links = []
+
         for td in tds:
             columns.append(td.text)
+        
+        for i in range(len(tds)):
+            td = tds[i]
+            link = ""
+            # I have to see what other data I can get from here!
+            #  get startup url if there is a url
+            if i == 1:
+                try:
+                    link = [x.get('href') for x in td.find_all('a')][0]
+                except:
+                    link = ""
+                links.append(link)
+
+            # Full Story link
+            if i == 4:
+                try: 
+                    link = [x.get('href') for x in td.find_all('a')][0]
+                except:
+                    link = "" 
+                links.append(link)
+
+            # Getting founder link
+            if i == 5:
+                try :   
+                    link = [x.get('href') for x in td.find_all('a')][0]
+                except:
+                    link = "" 
+                links.append(link)        
+
+        columns = columns + links
         dataset.append(columns)
     columns = dataset[1]
+    columns[6] = "Startup_link"
+    columns[7] = "Full_story_link"
+    columns[8] = "Founder_link"
+    columns = [x.replace(" ", "_") for x in columns]
     dataset = dataset[2:]
     return pd.DataFrame(dataset, columns=columns)
 
@@ -26,8 +63,8 @@ def get_df(tr_array):
 # ====================================================================
 def main():
     base = "http://autopsy.io"
-    directory_save = "datasets/"
-    file_name = "autopsyio_dataset.csv"
+    directory_save = "datasets/autopsy_io/"
+    file_name = "failed_companies_autopsyio_dataset.csv"
     r = requests.get(base)
     soup =  BeautifulSoup(r.text, 'html.parser')
     tr_array = soup.find_all('tr')
